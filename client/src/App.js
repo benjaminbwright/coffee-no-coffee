5b2a3bd75e0d8bf31b10c6821b260c76716f1085
Benjamin Wright <bwright@trilogyed.com>
2019-10-23 18:02:39 -0400

    Coffee buttons adjust odds by 1

--- client/src/App.js
+++ client/src/App.js
@@ -4,39 +4,55 @@ import './App.scss';
 
 console.log(coffeecup);
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    coffeeOdds: 25
+  }
 
   // Handle click event for the coffee is full icon.
-  const coffeeFullClick = () => {
-    console.log(`Yay! There's coffee.`);
+  coffeeFullClick = () => {
+    let coffeeOdds = this.state.coffeeOdds;
+    console.log(`Yay! There's coffee.`, coffeeOdds);
+    coffeeOdds++;
+    this.setState({
+      coffeeOdds: coffeeOdds
+    })
     // make an api call that tracks full coffee in the DB
   }
 
   // Handle click event for when the coffee is empty.
-  const coffeeEmptyClick = () => {
-    console.log(`There's no coffee. Come back later.`);
+  coffeeEmptyClick = () => {
+    let coffeeOdds = this.state.coffeeOdds;
+    console.log(`There's no coffee. Come back later`);
+    coffeeOdds--;
+    this.setState({
+      coffeeOdds: coffeeOdds
+    })
     // make an api call that tracks empty coffee in the DB
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Coffee No Coffee</h1>
-      </header>
-      <div id="coffee-prediction">
-        <div id="coffee-odds">
-          50%
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Coffee No Coffee</h1>
+        </header>
+        <div id="coffee-prediction">
+          <div id="coffee-odds">
+            { this.state.coffeeOdds }%
+          </div>
+          <h2>Chance of Coffee</h2>
         </div>
-        <h2>Chance of Coffee</h2>
-      </div>
-      <div id="footer-interface">
-        <div id="coffee-buttons">
-          <div className="coffee-button filled-up" onClick={coffeeFullClick}><img src={coffeecup} alt="Coffe Cup"/></div>
-          <div className="coffee-button poured-out" onClick={coffeeEmptyClick}><img src={coffeecup} alt="Coffe Cup"/></div>
+        <div id="footer-interface">
+          <div id="coffee-buttons">
+            <div className="coffee-button filled-up" onClick={this.coffeeFullClick}><img src={coffeecup} alt="Coffe Cup"/></div>
+            <div className="coffee-button poured-out" onClick={this.coffeeEmptyClick}><img src={coffeecup} alt="Coffe Cup"/></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
