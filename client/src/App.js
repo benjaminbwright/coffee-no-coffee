66dbb986111787def3f84fa11a6527ec21bbf59a
Benjamin Wright <bwright@trilogyed.com>
2019-10-23 17:38:53 -0400

    Added coffee is full

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import './App.css';
+import coffeecup from './big-cup.svg';
+import './App.scss';
+
+console.log(coffeecup);
 
 function App() {
+
+  // Handle click event for the coffee is full icon.
+  const coffeeFullClick = () => {
+    console.log(`Yay! There's coffee.`);
+    // make an api call that tracks full coffee in the DB
+  }
+
+  // Handle click event for when the coffee is empty.
+  const coffeeEmptyClick = () => {
+    console.log(`There's no coffee. Come back later.`);
+    // make an api call that tracks empty coffee in the DB
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Coffee No Coffee</h1>
       </header>
       <div id="coffee-prediction">
-        <h2>Chances of Coffee Right Now</h2>
         <div id="coffee-odds">
-          50% Chance of Coffee
+          50%
         </div>
+        <h2>Chances of Coffee Right Now</h2>
       </div>
       <div id="footer-interface">
         <div id="coffee-buttons">
-          <button>Coffee</button> | 
-          <button>No Coffee</button>
+          <div className="coffee-button filled-up" onClick={coffeeFullClick}><img src={coffeecup} alt="Coffe Cup"/></div>
+          <div className="coffee-button poured-out" onClick={coffeeEmptyClick}><img src={coffeecup} alt="Coffe Cup"/></div>
         </div>
       </div>
     </div>
