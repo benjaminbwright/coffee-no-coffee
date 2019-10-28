302d717188e4fbcd72daa9a9b79d8ae8467bdf8c
Benjamin Wright <bwright@trilogyed.com>
2019-10-28 15:44:31 -0400

    added route to server for logging coffe status

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const ROUTE = 4000;
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/coffeenocoffeedb", { useNewUrlParser: true });
 
-// the index route
+// the index route 
 // expect this route only to deliver dead air since it should not be publicly viewable
 app.get("/", (req, res) => {res.send("Yo")});
+
+app.get("/api/v1/coffeeStatus/:coffeePresent", (req, res) => {
+  res.send("You just updated the coffee status!!! Is there coffee? "+req.params.coffeePresent);
+});
 
 // TODO: add a general route the redirects all unauthorized to a 404 or something similar
 
